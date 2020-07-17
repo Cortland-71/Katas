37cf04bd7a7822628bff1af95c36ddf96d475d68
Cortland <cortland.carrillo.dev@gmail.com>
2020-07-16 19:18:27 -0700

    Finished Dup encoder kata

--- src/katas/strings/DuplicateEncoder.java
+++ src/katas/strings/DuplicateEncoder.java
@@ -1,5 +1,7 @@
 package katas.strings;
 
+import java.util.Arrays;
+
 //The goal of this exercise is to convert a string to a new 
 //string where each character in the new string is "(" if that 
 //character appears only once in the original string, or ")" if 
@@ -7,24 +9,27 @@ package katas.strings;
 //Ignore capitalization when determining if a character is a duplicate.
 
 public class DuplicateEncoder {
-	
-	public static void main(String[] args) {
-		System.out.println(encode("abcc"));
-	}
-	
-	public static String encode(String word){
-	    
-		StringBuilder sb = new StringBuilder();
-	    
-	    for (int i = 0; i < word.length(); i++) {
-	    	
-			for (int j = i+1; j < word.length(); j++) {
-				if(word.charAt(i) == word.charAt(j)) sb.append(")");
-				else sb.append("(");
-			}
-		}
-	    System.out.println(sb.toString());
-	    return "";
-	  }
 
+	public static void main(String[] args) {
+		System.out.println(encode("*8Vej1/*5"));
+		System.out.println(encode("abcdabbc"));
+		System.out.println(encode("(()(("));
+		System.out.println(encode("Prespecialized"));
+		System.out.println(encode("X7gl("));
+		System.out.println(encode(" ( ( )"));
+		System.out.println(encode("Supralapsarian"));
+	}
+
+	public static String encode(String word) {
+		word = word.toLowerCase();
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 0; i < word.length(); i++) {
+			boolean duplicateInFront = word.substring(i+1).contains(String.valueOf(word.charAt(i)));
+			boolean duplicateInTail = word.substring(0,i).contains(String.valueOf(word.charAt(i)));
+			boolean hasMultiple = duplicateInFront || duplicateInTail ? true : false;
+			sb.append(hasMultiple ? ')' : '(');
+		}
+		return sb.toString();
+	}
 }
